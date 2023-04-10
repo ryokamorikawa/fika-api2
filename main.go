07ee1eda67e5332aa69562dd9eca0f11d33e06c6
scsk-morikawa <r.morikawa@scsk.jp>
2023-04-10 15:20:08 +0900

    add sumple api

--- main.go
+++ main.go
@@ -1,34 +1,75 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	log.Print("starting server...")
-	http.HandleFunc("/", handler)
+// user represents data about a record user.
+type user struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Adress string  `json:"adress"`
+	Age    float64 `json:"age"`
+}
 
-	// Determine port for HTTP service.
+// users slice to seed record user data.
+var users = []user{
+	{ID: "1", Name: "佐藤　太郎", Adress: "北海道", Age: 56},
+	{ID: "2", Name: "佐川　春子", Adress: "東京", Age: 17},
+	{ID: "3", Name: "佐々木　聡", Adress: "静岡", Age: 39},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/users", getUsers)
+	router.GET("/users/:id", getUserByID)
+	router.POST("/users", postUsers)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("defaulting to port %s", port)
 	}
 
-	// Start HTTP server.
-	log.Printf("listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+	router.Run("0.0.0.0:" + port)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	name := os.Getenv("NAME")
-	if name == "" {
-		name = "Cloud Run 2"
+// getUsers responds with the list of all users as JSON.
+func getUsers(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, users)
+}
+
+// postUsers adds an user from JSON received in the request body.
+func postUsers(c *gin.Context) {
+	var newuser user
+
+	// Call BindJSON to bind the received JSON to
+	// newuser.
+	if err := c.BindJSON(&newuser); err != nil {
+		return
 	}
-	fmt.Fprintf(w, "Hello %s!\n", name)
+
+	// Add the new user to the slice.
+	users = append(users, newuser)
+	c.IndentedJSON(http.StatusCreated, newuser)
+}
+
+// getUserByID locates the user whose ID value matches the id
+// parameter sent by the client, then returns that user as a response.
+func getUserByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of users, looking for
+	// an user whose ID value matches the parameter.
+	for _, a := range users {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "user not found"})
 }
